0c3bec3f3ed141a2d4bc76c09c847a6056538efd
ishaq Nasir <ishaqnasir47@gmail.com>
2021-04-12 05:43:12 +0800

    operator

---
+++ Operator/basics.py
@@ -0,0 +1,97 @@
+print(20 * 5)
+
+
+   +  Addition
+
+   -   Subtraction
+
+   *   Multiplication
+
+   /    Division
+
+  //    Floor division
+
+ **    Exponential
+
+ %     Young modulus
+
+
+
+x + y           
+print(2 + 3)
+
+x - y 
+print(4 - 2)
+
+x * y
+print(4 * 2)
+
+x / y
+print(4 / 2)
+
+x // y
+print(4 // 2)    
+#this round the division down to the nearest whole number
+
+x ** y
+print(4 ** 2)   
+
+x % y
+print(4 % 2)    
+#this outputs the reminder
+
+
+
+
+ =
+ x = 7   
+ #this means x is equal to 7
+
+
+
+
++=
+#if this applied to any value it means there is an addition in the variable , let us see some example below
+
+x = 7
+
+x += 3
+
+#the output of this would be 10 meaning 7 plus 3 
+-----------------------------------------------------
+-=
+#a vice versa of the above one
+
+x = 7
+x -= 3
+#the output will be 4 as it subtracts 7 from 3
+-----------------------------------------------------
+*=
+#multiply assignment
+
+x = 7
+x *= 3
+#the output will be 21 as it multiply 7 by 3
+-----------------------------------------------------
+/=
+#divisible assignment
+
+x = 10
+x /= 2
+#the output will be 5. as it divides 10 by 
+-----------------------------------------------------
+//=
+#rounding up the divisible assignment
+
+x = 7
+x //= 3
+#the output will be 2 as it divides 7 by 3 and it rounds it up to the nearest whole number
+-----------------------------------------------------
+%=
+#a vice versa of the above one
+
+x = 7
+x %= 3
+#the output will be 1 as it gets the remainder
+
+
